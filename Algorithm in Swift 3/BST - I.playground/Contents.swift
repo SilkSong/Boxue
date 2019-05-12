6548258160158cda20d02496b6703e663beb28e1
Silk Song <szxsilk@gmail.com>
2019-05-12 20:35:48 +0800

    BST init and insert

---
+++ Algorithm in Swift 3/BST - I.playground/Contents.swift
@@ -0,0 +1,60 @@
+import UIKit
+
+open class TreeNode<T: Comparable> {
+    fileprivate var value: T
+    fileprivate var parent: TreeNode?
+    fileprivate var left: TreeNode?
+    fileprivate var right: TreeNode?
+    
+    public init(value: T, parent: TreeNode? = nil, left: TreeNode? = nil, right: TreeNode? = nil) {
+        self.value = value
+        self.parent = parent
+        self.left = left
+        self.right = right
+    }
+
+}
+
+open class BST<T: Comparable> {
+    fileprivate(set) public var root: TreeNode<T>?
+    
+    public init(_ value: T) {
+        self.root = TreeNode<T>(value: value)
+    }
+    
+    public init(_ array: [T]) {
+        precondition(array.count > 0)
+        
+        self.root = TreeNode<T>(value: array[0])
+        
+        for value in array[1 ..< array.count] {
+            insert(value: value)
+        }
+    }
+    
+    open func insert(value: T) {
+        self.insert(value: value, under: self.root!)
+    }
+}
+
+extension BST {
+    fileprivate func insert(value: T, under parent: TreeNode<T>) {
+        if value < parent.value {
+            if let left = parent.left {
+                insert(value: value, under: left)
+            } else {
+                let node = TreeNode<T>(value: value)
+                parent.left = node
+                node.parent = parent
+            }
+        } else if value > parent.value {
+            if let right = parent.right {
+                insert(value: value, under: right)
+            } else {
+                let node = TreeNode<T>(value: value)
+                parent.right = node
+                node.parent = parent
+            }
+        }
+    }
+}
